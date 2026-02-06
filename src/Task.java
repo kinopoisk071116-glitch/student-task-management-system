61163ed3cd8e58837b14fc24e3746a9bad29df9f
kinopoisk071116-glitch <kinopoisk071116@gmail.com>
2026-02-06 23:41:28 +0500

    Add Task entity

---
+++ src/Task.java
@@ -0,0 +1,12 @@
+public class Task {
+
+    private String title;
+
+    public Task(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
